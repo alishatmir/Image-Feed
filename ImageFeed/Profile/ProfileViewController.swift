128f622269cb7ec33082515d1907c932ca2668c3
Alina Tikhomirova <alinatmir@gmail.com>
2025-06-02 10:42:30 +0700

    sprint_09_complete

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,15 +1,120 @@
 import UIKit
 
-final class ProfileViewController: UITableViewCell {
+final class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var userPhoto: UIImageView!
+    private var userPhotoImageView: UIImageView = {
+        let profileImage = UIImage(systemName: "person.crop.circle.fill")
+        let imageView = UIImageView(image: profileImage)
+        let image = UIImage(named: "userPhoto")
+        imageView.image = image
+        imageView.backgroundColor = .ypBlack
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
-    @IBOutlet weak var loginLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var discriptionLabel: UILabel!
-    @IBOutlet weak var logoutButton: UIButton!
+    private var loginLabel: UILabel = {
+        let loginLabel = UILabel()
+        loginLabel.text = "Екатерина Новикова"
+        loginLabel.textColor = .ypWhite
+        let font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        loginLabel.font = font
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        return loginLabel
+    }()
     
+    private var emailLabel: UILabel = {
+        let emailLabel = UILabel()
+        emailLabel.text = "@ekaterina_nov"
+        emailLabel.textColor = .ypWhite
+        let font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        emailLabel.font = font
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        return emailLabel
+    }()
     
-    @IBAction func didTapLogoutButton(_ sender: Any) {
+    private var descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello"
+        descriptionLabel.textColor = .ypWhite
+        let font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionLabel
+    }()
+    
+    lazy var forwardButton: UIButton = {
+        let forwardButton = UIButton(type: .custom)
+        forwardButton.setImage(.init(resource: .exit), for: .normal)
+        forwardButton.addTarget(
+            self,
+            action: #selector(didTapLogoutButton),
+            for: .touchUpInside
+        )
+        forwardButton.translatesAutoresizingMaskIntoConstraints = false
+        forwardButton.tintColor = UIColor.ypRed
+        return forwardButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addUserPhotoImageView()
+        addLoginLabel()
+        addEmailLabel()
+        addDescriptionLabel()
+        addForwardButton()
+    }
+    
+    func addUserPhotoImageView() {
+        view.addSubview(userPhotoImageView)
+        
+        NSLayoutConstraint.activate([
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 70),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 70),
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        ])
+    }
+    
+    func addLoginLabel(){
+        view.addSubview(loginLabel)
+        
+        NSLayoutConstraint.activate([
+            loginLabel.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: 8),
+            loginLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.leadingAnchor)
+        ])
+    }
+    
+    func addEmailLabel() {
+        view.addSubview(emailLabel)
+        
+        NSLayoutConstraint.activate([
+            emailLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            emailLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.leadingAnchor)
+        ])
+    }
+    
+    func addDescriptionLabel() {
+        view.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.leadingAnchor)
+        ])
+    }
+    
+    func addForwardButton() {
+        view.addSubview(forwardButton)
+        
+        NSLayoutConstraint.activate([
+            forwardButton.centerYAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor),
+            forwardButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            forwardButton.widthAnchor.constraint(equalToConstant: 44),
+            forwardButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    @objc
+    func didTapLogoutButton() {
+        
     }
 }
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -1,11 +1,118 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
-    var image: UIImage?
     
-    @IBOutlet private var imageView: UIImageView!
+    var scrollView = UIScrollView()
+    var imageView = UIImageView()
+    
+    lazy var backwardButton: UIButton = {
+        let backwardButton = UIButton(type: .custom)
+        backwardButton.setImage(.init(resource: .backward), for: .normal)
+        backwardButton.addTarget(
+            self,
+            action: #selector(didTapBackwardButton),
+            for: .touchUpInside
+        )
+        backwardButton.translatesAutoresizingMaskIntoConstraints = false
+        return backwardButton
+    }()
+    
+    lazy var shareButton: UIButton = {
+        let sharingButton = UIButton(type: .custom)
+        sharingButton.setImage(.init(resource: .sharing), for: .normal)
+        sharingButton.addTarget(
+            self,
+            action: #selector(didTapShareButton),
+            for: .touchUpInside
+        )
+        sharingButton.translatesAutoresizingMaskIntoConstraints = false
+        sharingButton.tintColor = UIColor.ypRed
+        return sharingButton
+    }()
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded, let image else { return }
+            
+            imageView.image = image
+            imageView.frame.size = image.size
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         imageView.image = image
+        guard let image = image else { return }
+        imageView.frame.size = image.size
+        
+        createScrollView()
+        scrollView.delegate = self
+        createButtons()
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    func createScrollView() {
+        scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.maximumZoomScale = 1.25
+        scrollView.minimumZoomScale = 0.1
+        scrollView.addSubview(imageView)
+        scrollView.contentSize = self.imageView.bounds.size
+        self.view.addSubview(scrollView)
+        view.backgroundColor = .ypBlack
+    }
+    
+    func createButtons() {
+        view.addSubview(backwardButton)
+        view.addSubview(shareButton)
+        NSLayoutConstraint.activate([
+            backwardButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 9),
+            backwardButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            backwardButton.widthAnchor.constraint(equalToConstant: 24),
+            backwardButton.heightAnchor.constraint(equalToConstant: 24),
+            
+            shareButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -17),
+            shareButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shareButton.widthAnchor.constraint(equalToConstant: 50),
+            shareButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let maximumZoomScale = scrollView.maximumZoomScale
+        let minimumZoomScale = scrollView.minimumZoomScale
+        
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let hScale = visibleRectSize.width / image.size.width
+        let vScale = visibleRectSize.height / image.size.height
+        let scale = min(maximumZoomScale, max(minimumZoomScale, min(hScale, vScale)))
+        self.scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    @objc func didTapBackwardButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapShareButton() {
+        guard let image else { return }
+        let sharing = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(sharing, animated: true, completion: nil)
+    }
+}
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming( in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
